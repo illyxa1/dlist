42a35a9457784db92cdfaaecc389e52956fc6e1c
Ilya <63089397+illyxa1@users.noreply.github.com>
2024-11-25 13:49:18 +0300

    updated some logic, manual i will update later

--- d_list.c
+++ d_list.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "d_list.h"
+#include "error.h"
 
 // dlist - struckt, what must to contain link("link") on dynamic list with size ("size") of type with "type_size"
 //-= FUNCTIONAL =------------------------------------------------------------------------------------------------------------------------------------
 size_t correct_size(float size);
 //-= GLOBAL =----------------------------------------------------------------------------------------------------------------------------------------
-int init_dlist(struct dlist *dlist_link, size_t size, size_t type_size)
+int init_dlist(Dlist *dlist_link, size_t size, size_t type_size)
 {// initalisates dlist, sets sizes, alocates memory for array;
 	if (dlist_link == NULL)
-		return 2;
+		error(E_INVALID_POINTER, __FILE__, __LINE__);
 
 	dlist_link -> size = correct_size(size);
+	dlist_link -> pseudo_size = size;
 	dlist_link -> at = 0;
 	dlist_link -> type_size = type_size;
 
@@ -19,26 +22,31 @@ int init_dlist(struct dlist *dlist_link, size_t size, size_t type_size)
 	return 0;
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-int prepare_dlist(struct dlist *dlist_link)
+int prepare_dlist(Dlist *dlist_link)
 {
 	void *new_place;
 
 	if (dlist_link == NULL)
-		return 2;
+		error(E_INVALID_POINTER, __FILE__, __LINE__);
 
 	if ((dlist_link -> at) >= (dlist_link -> size)) // if array too small
 	{
-		if (dlist_link -> size < MAXINC)dlist_link -> size *= 2;
+		if (dlist_link -> size < MAXINC)
+		{
+			dlist_link -> size *= 2;
+			dlist_link -> pseudo_size ++;
+		}
 		else dlist_link -> size += MAXINC; // if array to big to incrase it 2 times
 
 		new_place = malloc(dlist_link -> type_size * (dlist_link -> size)); // void** on new place void* 
+		memcpy(new_place, dlist_link -> link, dlist_link -> type_size * dlist_link -> size);
 		free(dlist_link -> link);
 		dlist_link -> link = new_place; // redirection of array
 	}// else everything is okay
 	return 0;
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------
-int is_dlist_overflow(struct dlist *dlist_link)
+int is_dlist_overflow(Dlist *dlist_link)
 {
 	if ((dlist_link -> at) < (dlist_link -> size)) // if pointer on last smaller, than last 
 		return 0;
--- d_list.h
+++ d_list.h
@@ -1,22 +1,31 @@
-#define get(DLIST, AT, TYPE) (AT < DLIST.size)? (*(( (TYPE*) DLIST.link ) + AT)) : 0// : error(OWERFLOW) // macros, what makes new abstraction above dlist reading
-#define set(DLIST, AT, TYPE, VALUE) (AT < DLIST.size)? (*(( (TYPE*) DLIST.link ) + AT)) = VALUE : 0// : error(OWERFLOW) // macros, what makes new abstraction above dlist writing
+#define get(DLIST, AT, TYPE) (AT < DLIST.size)? (*(( (TYPE*) DLIST.link ) + AT)) : \
+		error(E_INVALID_LIST_ELEMENT, __FILE__, __LINE__)
+ // macros, what makes new abstraction above dlist reading
+ //
+#define set(DLIST, AT, TYPE, VALUE) (AT < DLIST.size)? (*(( (TYPE*) DLIST.link ) + AT)) = VALUE : \
+		error(E_INVALID_LIST_ELEMENTT, __FILE__, __LINE__)
+ // macros, what makes new abstraction above dlist writing
+ //
 #define append(DLIST, TYPE, VALUE) (DLIST.at >= DLIST.size)? 								\
 					(prepare_dlist(&DLIST), *(( (TYPE*) DLIST.link ) + DLIST.at++) = VALUE): \
-				   	(*(( (TYPE*) DLIST.link ) + DLIST.at++) = VALUE)
-
+				   	(*(( (TYPE*) DLIST.link ) + DLIST.at++) = VALUE, (DLIST.pseudo_size < DLIST.at)?\
+					 DLIST.pseudo_size++ :0)// error(E_OK, __FILE__, __LINE__))
+				   
 #define MAXINC 64
 //-= STRUCTS =---------------------------------------------------------------------------------------------------------------------------------------
 struct dlist{
 	size_t at;
 	size_t size;
+	size_t pseudo_size;
 	size_t type_size;
 	void *link;
 
 };
+typedef struct dlist Dlist;
 //-= DEFS =------------------------------------------------------------------------------------------------------------------------------------------
-int init_dlist(struct dlist *dlist_link, size_t size, size_t type_size); 
+int init_dlist(Dlist *dlist_link, size_t size, size_t type_size); 
 // initalise dlist of objects of "type_size" with size "size", and stores it to pointer on void in "link"
-int prepare_dlist(struct dlist *dlist_link);
+int prepare_dlist(Dlist *dlist_link);
 // if dlist owerfowed, prepaires it to adding new data
-int is_dlist_overflow(struct dlist *dlist_link);
+int is_dlist_overflow(Dlist *dlist_link);
 // checks on dlist overflowing
